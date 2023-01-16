418a55d1f6ac43d8b5184759e925d1217de24939
tsutskiridze98 <cucqiridze.lado@gmail.com>
2023-01-17 00:29:42 +0400

    mitsis kat

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ shenishvnebi.forEach((el) => {
     const htmlShenishvnaOptions = `<option value="${el}"></option>`;
     shenishvna.insertAdjacentHTML('beforeend', htmlShenishvnaOptions);
 });
+
+
+/////////// მიწის კატეგორია /////////////////////
+const categoryLand = document.getElementById('miwis_kategoria');
+const arr = [{num: "ერთი", letter: 'A'}, {num: "ორი", letter: 'B'}, {num: "სამი", letter: "C"}];
+
+arr.forEach(number => {
+    const htmlNumber = `<option value="${number.letter}">${number.num}</option>`;
+    categoryLand.insertAdjacentHTML('beforeend', htmlNumber);
+})
